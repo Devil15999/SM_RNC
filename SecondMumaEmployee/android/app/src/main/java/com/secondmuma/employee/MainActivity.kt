2d07edf477946a21a2a6c06ffceff244ca704b46
Devil15999 <bharathkr848@gmail.com>
2026-06-11 14:42:11 +0530

    employee App package update

--- SecondMumaEmployee/android/app/src/main/java/com/secondmuma/employee/MainActivity.kt
+++ SecondMumaEmployee/android/app/src/main/java/com/secondmuma/employee/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.secondmuma
+package com.secondmuma.employee
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
--- SecondMumaEmployee/android/app/src/main/java/com/secondmuma/employee/MainApplication.kt
+++ SecondMumaEmployee/android/app/src/main/java/com/secondmuma/employee/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.secondmuma
+package com.secondmuma.employee
 
 import android.app.Application
 import com.facebook.react.PackageList
